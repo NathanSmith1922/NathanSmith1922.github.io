251f3f797dd0599a496f40ae019fd398f026526a
Nathan Smith <nathan.smith1922@gmail.com>
2023-05-13 00:57:24 +1000

    animations

--- Portfolio/main.js
+++ Portfolio/main.js
@@ -1,24 +1,51 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show');
+    }
+  });
+});
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const observerGrid = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    console.log(entry);
+    if (entry.isIntersecting) {
+      entry.target.classList.add('show-grid');
+    }
+  });
+});
+const hiddenElementsGrid = document.querySelectorAll('.hidden-grid');
+hiddenElementsGrid.forEach((el) => observerGrid.observe(el));
 
-setupCounter(document.querySelector('#counter'))
+/*    HEADER BUTTONS    */
+
+let aboutButton = document.getElementById('about-button');
+let aboutSection = document.getElementById('about-section');
+aboutButton.onclick = function() {
+  console.log("I was clicked!");
+  aboutSection.scrollIntoView({behavior: 'smooth'});
+}
+
+let projectsButton = document.getElementById('projects-button');
+let projectsSection = document.getElementById('projects-section');
+projectsButton.onclick = function() {
+  console.log("I was clicked!");
+  projectsSection.scrollIntoView({behavior: 'smooth'});
+}
+
+let skillsButton = document.getElementById('skills-button');
+let skillsSection = document.getElementById('skills-section');
+skillsButton.onclick = function() {
+  console.log("I was clicked!");
+  skillsSection.scrollIntoView({behavior: 'smooth'});
+}
+
+let contactButton = document.getElementById('contact-button');
+let contactSection = document.getElementById('contact-section');
+contactButton.onclick = function() {
+  console.log("I was clicked!")
+  contactSection.scrollIntoView({behavior: 'smooth'});
+}
